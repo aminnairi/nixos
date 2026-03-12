1cf51251c38104cd27706952447905c0ac1df63a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-12 11:06:31 +0100

    fixed config for the version 25.11

--- configuration.nix
+++ configuration.nix
@@ -2,164 +2,168 @@
 
 let
 
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
 
 in
 
-  { imports =
-    [ /etc/nixos/hardware-configuration.nix
-    (import "${home-manager}/nixos")
-  ];
+{ imports =
+	[ /etc/nixos/hardware-configuration.nix
+		(import "${home-manager}/nixos")
+	];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+	boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  powerManagement.enable = true;
+	powerManagement.enable = true;
 
-  time.timeZone = "Europe/Paris";
+	time.timeZone = "Europe/Paris";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  services = {
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
-    };
-    printing = {
-      enable = true;
-      drivers = with pkgs; [
-        gutenprint 
-      ];
-    };
-  };
+	services = {
+		avahi = {
+			enable = true;
+			nssmdns4 = true;
+			openFirewall = true;
+		};
+		printing = {
+			enable = true;
+			drivers = with pkgs; [
+				gutenprint 
+			];
+		};
+	};
 
-  hardware = {
-    sane = {
-      enable = true;
-      extraBackends = with pkgs; [
-        sane-airscan
-      ];
-    };
-  };
+	hardware = {
+		sane = {
+			enable = true;
+			extraBackends = with pkgs; [
+				sane-airscan
+			];
+		};
+	};
 
-  networking = {
-    hostName = "nixos";
-    firewall = {
-      enable = true;
-      allowPing = false;
-      allowedTCPPorts = [];
-      allowedUDPPorts = [];
-    };
-    networkmanager = {
-      enable = true;
-      wifi = {
-        powersave = true;
-      };
-    };
-  };
+	networking = {
+		hostName = "nixos";
+		firewall = {
+			enable = true;
+			allowPing = false;
+			allowedTCPPorts = [];
+			allowedUDPPorts = [];
+		};
+		networkmanager = {
+			enable = true;
+			wifi = {
+				powersave = true;
+			};
+		};
+	};
 
-  system.stateVersion = "25.05";
+	system.stateVersion = "25.05";
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = _: true;
-    };
-  };
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+			allowUnfreePredicate = _: true;
+		};
+	};
 
-  services = {
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "us"; # Ou votre disposition habituelle
-        options = "compose:ralt"; # Définit Alt Gr comme touche de composition
-      };
-      displayManager = {
-        gdm = {
-          enable = true;
-        };
-      };
-      desktopManager = {
-        gnome = {
-          enable = true;
-        };
-      };
-    };
-  };
+	services = {
+		displayManager = {
+			gdm = {
+				enable = true;
+			};
+		};
+		desktopManager = {
+			gnome = {
+				enable = true;
+			};
+		};
+		xserver = {
+			enable = true;
+			xkb = {
+				layout = "us"; # Ou votre disposition habituelle
+					options = "compose:ralt"; # Définit Alt Gr comme touche de composition
+			};
+		};
+	};
 
-  programs = {
-    fish = {
-      enable = true;
-    };
+	programs = {
+		fish = {
+			enable = true;
+		};
 
-    git = {
-      enable = true;
-      config = {
-        init = {
-          defaultBranch = "development";
-        };
-        user = {
-          name = "aminnairi";
-          email = "18418459+aminnairi@users.noreply.github.com";
-        };
-      };
-    };
+		git = {
+			enable = true;
+			config = {
+				init = {
+					defaultBranch = "development";
+				};
+				user = {
+					name = "aminnairi";
+					email = "18418459+aminnairi@users.noreply.github.com";
+				};
+			};
+		};
 
-    vim = {
-      enable = true;
-      defaultEditor = true;
-    };
-  };
+		vim = {
+			enable = true;
+			defaultEditor = true;
+		};
 
-  environment = {
-    variables = {
-      EDITOR = "vim";
-    };
-    systemPackages = with pkgs; [
-      git
-      vim
-      fish
-      google-chrome
-      home-manager
-    ];
-    gnome = {
-      excludePackages = [
-        pkgs.geary
-        pkgs.gnome-calendar
-        pkgs.gnome-contacts
-        pkgs.gnome-clocks
-        pkgs.snapshot
-        pkgs.gnome-tour
-        pkgs.gnome-text-editor
-        pkgs.gnome-weather
-        pkgs.gnome-maps
-        pkgs.gnome-music
-      ];
-    };
-  };
+		dconf = {
+			enable = true;
+			profiles = {
+				user = {
+					databases = [
+					{
+						settings = {
+							"org/gnome/desktop/input-sources" = {
+								xkb-options = [ "compose:ralt" ];	
+							};
+						};
+					}
+					];
+				};
+			};
+		};
+	};
 
-  users.users.amin = {
-    isNormalUser = true;
-    description = "Amin NAIRI";
-    extraGroups = [ "video" "wheel" ];
-    shell = pkgs.fish;
-    home = "/home/amin";
-  }; 
+	environment = {
+		variables = {
+			EDITOR = "vim";
+		};
+		systemPackages = with pkgs; [
+			git
+				vim
+				fish
+				google-chrome
+				home-manager
+		];
+		gnome = {
+			excludePackages = [
+				pkgs.geary
+					pkgs.gnome-calendar
+					pkgs.gnome-contacts
+					pkgs.gnome-clocks
+					pkgs.snapshot
+					pkgs.gnome-tour
+					pkgs.gnome-text-editor
+					pkgs.gnome-weather
+					pkgs.gnome-maps
+					pkgs.gnome-music
+			];
+		};
+	};
 
-  home-manager.users.amin = { pkgs, ... }: {
-    home = {
-      stateVersion = "25.05";
-    };
-
-    dconf.settings = {
-      "org/gnome/desktop/input-sources" = {
-        xkb-options = [ "compose:ralt" ];
-      };
-    };
-
-  };
+	users.users.amin = {
+		isNormalUser = true;
+		description = "Amin NAIRI";
+		extraGroups = [ "video" "wheel" ];
+		shell = pkgs.fish;
+		home = "/home/amin";
+	}; 
 }
 
