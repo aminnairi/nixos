a9830a99a743ca0dbbc8f1323f584cfce85ad530
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-22 20:35:27 +0100

    added revision for lazyvim

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,7 @@ in
   home-manager.users.amin.xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
     owner = "LazyVim";
     repo = "Starter";
+    rev = "803bc181d7c0d6d5eeba9274d9be49b287294d99";
   };
 
   home-manager.users.amin.xdg.configFile."nvim/lua/config/plugins/avante.lua".text = ''
