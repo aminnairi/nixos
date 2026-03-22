ccdc5d7a15980ab54de39ef73159c684f9173bce
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-22 21:03:01 +0100

    new attempt at setting for avante.nvim

--- configuration.nix
+++ configuration.nix
@@ -128,14 +128,8 @@ in
     sha256 = "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM=";
   };
 
-  home-manager.users.amin.xdg.configFile."test".text = "Hello, world!";
+  home-manager.users.amin.xdg.configFile."nvim/lua/config/plugins/avante.lua".text = "return { import = 'lazyvim.plugins.extras.ai.avante' }";
 
-  # home-manager.users.amin.home.file."nvim/lua/config/plugins/avante.lua".text = ''
-  #   return {
-  #     import = "lazyvim.plugins.extras.ai.avante"
-  #   }
-  # '';
-  #
 # Kitty
   home-manager.users.amin.programs.kitty.enable = true;
   home-manager.users.amin.programs.kitty.enableGitIntegration = true;
