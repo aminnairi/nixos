f9a8a13a8c6e75fb77bf42d7f95974924d651516
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-22 22:09:53 +0100

    fixed import path for nixvim

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@ in
 {
   imports = [
     (import "${home-manager}/nixos")
-    (import "${nixvim}/wrappers/hm")
+    (import "${nixvim}/wrappers/hm.nix")
   ];
 
   nixpkgs.pkgs = import nixpkgs-src {
