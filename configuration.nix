2e9efdf9bbe930c6e1975d4a25d3329da50bdcf7
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-22 21:07:53 +0100

    fixed issue while installing lazyvim plugin files

--- configuration.nix
+++ configuration.nix
@@ -128,6 +128,7 @@ in
     sha256 = "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM=";
   };
 
+  home-manager.users.amin.xdg.configFile."nvim".recursive = true;
   home-manager.users.amin.xdg.configFile."nvim/lua/config/plugins/avante.lua".text = "return { import = 'lazyvim.plugins.extras.ai.avante' }";
 
 # Kitty
