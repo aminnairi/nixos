8c4f6056947316f5ed9997304f7d8f74e7f20834
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-03-22 10:00:49 +0100

    added bunch of other programs for home manager

--- configuration.nix
+++ configuration.nix
@@ -1,54 +1,69 @@
 { config, lib, pkgs, ... }:
+
 let
-  nixpkgs-src = builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/nixos-25.11.tar.gz;
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
+nixpkgs-src = builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/nixos-25.11.tar.gz;
+home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
 in
-{ 
+{
   imports = [
     /etc/nixos/hardware-configuration.nix
-    (import "${home-manager}/nixos")
+      (import "${home-manager}/nixos")
   ];
 
   nixpkgs.pkgs = import nixpkgs-src {
     config.allowUnfree = true;
   };
 
+# System state
+  system.stateVersion = "25.11";
+
+# System
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+# Power management
   powerManagement.enable = true;
 
+# Zram
   zramSwap.enable = true;
 
+# Time zone
   time.timeZone = "Europe/Paris";
 
+# Locale
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.defaultCharset = "UTF-8";
 
+# Networking
   networking.hostName = "nixos";
 
-  system.stateVersion = "25.05";
-
+# Users
+  users.users.amin.isNormalUser = true;
   users.users.amin.isNormalUser = true;
   users.users.amin.description = "Amin NAIRI";
-  users.users.amin.extragroups = [ "video" "wheel" "networkmanager" ];
+  users.users.amin.extraGroups = [ "video" "wheel" "networkmanager" ];
   users.users.amin.shell = pkgs.fish;
   users.users.amin.home = "/home/amin";
 
+# Fonts
   fonts.packages = with pkgs; [ nerd-fonts-jetbrains-mono ];
   fonts.fontconfig.enable = true;
   fonts.fontconfig.defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
 
-  programs.home-manager.enable = true;
-
-  home-manager.userUserPackages = true;
+# Home Manager
   home-manager.useGlobalPkgs = true;
+  home-manager.users.amin.home.stateVersion = "25.11";
 
-  home.username = "amin";
-  home.homeDirectory = "/home/amin";
-  home.stateVersion = "25.11";
-  home.programs.chromium.enable = true;
+# Neovim
+  home-manager.users.amin.programs.neovim.enable = true;
+  home-manager.users.amin.programs.neovim.defaultEditor = true;
 
-  wayland.windowManager.hyprland.enable = true;
+# Fish
+  home-manager.users.amin.programs.fish.enable = true;
+  home-manager.users.amin.programs.fish.completions.dkcpu = "docker compose up --detach --build";
+  
+# Home manager packages
+  home-manager.users.amin.home.packages = with pkgs; [
+    chromium
+  ];
 }
